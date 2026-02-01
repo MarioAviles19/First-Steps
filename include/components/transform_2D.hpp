aff92041566d3653a69e964624e1f7e675be99e2
Mario Aviles <mjaviles360@gmail.com>
2026-02-01 01:36:47 -0600

    feat: added transform functionality

--- include/components/transform_2D.hpp
+++ include/components/transform_2D.hpp
@@ -6,10 +6,10 @@ namespace game
 {
   class Transform2D : public game::Component{
     public:
-      glm::vec2 position;
-      glm::vec2 scale;
+      glm::vec2 position = glm::vec2(0.0f, 0.0f);
+      glm::vec2 scale = glm::vec2(1.0f, 1.0f);
       glm::vec2 size;
-      float rotation;
+      float rotation = 0.0f;
       glm::mat4 getModel();
       Transform2D(game::Entity& parent);
 
--- src/transform_2D.cpp
+++ src/transform_2D.cpp
@@ -1,9 +1,21 @@
 #include "components/transform_2D.hpp"
 #include "component.hpp"
+#include <glm/fwd.hpp>
 
 namespace game
 {
   Transform2D::Transform2D(game::Entity& parent) : game::Component(parent){
 
   };
+  glm::mat4 Transform2D::getModel()
+  {
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(position, 0.0f));
+    model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 0.0f, 1.0f));
+    model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f));
+    model = glm::scale(model, glm::vec3(scale, 1.0f));
+
+    return model;
+  }
 }
