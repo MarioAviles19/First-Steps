b5590e74ea3fdc1f231e93d34f4d3f96c9a48f3c
Mario Aviles <mjaviles360@gmail.com>
2026-02-01 01:37:21 -0600

    fix: moved template function to header

--- include/entity.hpp
+++ include/entity.hpp
@@ -13,9 +13,14 @@ namespace game{
       game::Transform2D transform;
       Entity();
 
-      template<typename T>
+      template <typename T>
         requires std::derived_from<T, game::Component>
-      T& addComponent();
+        T& addComponent()
+        {
+          auto& ptr = components.emplace_back(std::make_unique<T>(*this));
+          return static_cast<T&>(*ptr);
+        }
+
     private:
       std::vector<std::unique_ptr<game::Component>> components;
   };
--- src/entity.cpp
+++ src/entity.cpp
@@ -7,11 +7,4 @@ namespace game
   Entity::Entity() : transform(*this){
 
   }
-  template <typename T>
-    requires std::derived_from<T, game::Component>
-    T& Entity::addComponent()
-    {
-      auto& ptr = components.emplace_back(std::make_unique<T>(*this));
-      return static_cast<T&>(ptr);
-    }
-}
+  }
