31a4b5a07e2b2e51f2c0932c89c3c7968036fd64
Mario Aviles <mjaviles360@gmail.com>
2026-02-01 14:16:22 -0600

    fix: made class more type agnostic
    It should be able to store non-pointers as well

--- include/util/generational_vector.hpp
+++ include/util/generational_vector.hpp
@@ -8,14 +8,42 @@ namespace utils
     uint32_t index;
     uint32_t generation;
   };
-    template <typename T>
+  template <typename T>
     class GenerationalVector{
       public:
         GenerationalVector();
-        GenVectorHandle add(T* element);
-        void remove(GenVectorHandle handle);
+        GenVectorHandle add(T element){
+          if(!this->freeList.empty())
+          {
+            uint32_t idx = freeList.back();
+            this->freeList.pop_back();
+
+            this->elements[idx] = element; 
+            return {idx, this->generations[idx]};
+          }
+          uint32_t idx = this->elements.size();
+          this->elements.push_back(element);
+          this->generations.push_back(0);
+          return {idx, 0};
+  
+
+        }
+        void remove(GenVectorHandle handle){
+          if(handle.index >= this->elements.size())
+          {
+            return;
+          }
+          if(this->generations[handle.index] != handle.generation)
+          {
+            return;
+          }
+          this->elements[handle.index] = nullptr;
+          this->generations[handle.index]++;
+          this->freeList.push_back(handle.index);
+
+        }
       private:
-        std::vector<T*> elements;
+        std::vector<T> elements;
         std::vector<uint32_t> generations;
         std::vector<uint32_t> freeList;
     };
