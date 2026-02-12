965e81244bf16b238daf6a585e431fcb925000cc
Mario Aviles <mjaviles360@gmail.com>
2026-02-12 01:03:27 -0600

    feat: added save function

---
+++ src/scene.cpp
@@ -0,0 +1,21 @@
+#include "scene.hpp"
+#include "cereal/archives/json.hpp"
+#include "program.hpp"
+#include <fstream>
+#include <string>
+
+
+namespace game
+{
+  Scene::Scene(game::Program* program, std::string id): program(program), id(id){
+
+  }
+  void Scene::save(std::string path)
+  {
+    std::ofstream filePath(path);
+
+    cereal::JSONOutputArchive archive(filePath);
+
+    archive(this);
+  }
+}
