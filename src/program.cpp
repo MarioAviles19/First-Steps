2d3f47208dfa7d955bdb202893cb13edf8b85199
Mario Aviles <mjaviles360@gmail.com>
2026-03-04 00:07:27 -0600

    feat: started testing with program

--- src/program.cpp
+++ src/program.cpp
@@ -1,5 +1,8 @@
-
+#include "components/sprite_renderer.hpp"
 #include "program.hpp"
+#include <cereal/types/vector.hpp>
+#include "cereal/archives/json.hpp"
+#include "entity.hpp"
 #include "renderer.hpp"
 #include "resource_manager.hpp"
 #include "window.hpp"
@@ -9,17 +12,23 @@
 #include "input.hpp"
 namespace game
 {
-  std::string cheese = "cheese";
+  std::vector<std::unique_ptr<Entity>> entitiesForTesting;
   void Program::init()
   {
-      std::cout << cheese << std::endl;
     auto* win = new game::Window(800, 600, "First Steps");
     this->renderer = std::make_shared<Renderer>(win);
 
     ResourceManager& resourceManager = ResourceManager::GetInstance();
     resourceManager.RegisterResource<Renderer>(renderer);
+    std::unique_ptr cube = std::make_unique<Entity>();
+
+    cube->transform->scale = glm::vec2(1.0f, 1.0f);
+
+    
+    SpriteRenderer& Renderer1 = cube->addComponent<SpriteRenderer>(); 
+    entitiesForTesting.push_back(std::move(cube));
+
     glfwSetKeyCallback(this->renderer->window->glfwWindow, game::Input::glfwKeyCallback);
-    cheese = "gouda";
   }
   void Program::gameloop()
   {
@@ -36,9 +45,10 @@ namespace game
 
 
 
-      //glfwSwapBuffers(this->renderer->window->glfwWindow);
-      //glfwPollEvents();
     }
+    cereal::JSONOutputArchive oArchive(std::cout);
+    oArchive(entitiesForTesting);
+
 
 
   }
