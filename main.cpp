8029dd431be878feef18e43d497a908bcc0c44c9
Mario Aviles <mjaviles360@gmail.com>
2026-03-04 00:07:10 -0600

    feat: clean up main

--- main.cpp
+++ main.cpp
@@ -3,116 +3,31 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <iostream>
 #include <memory>
+#include <vector>
 
+#include "cereal/archives/json.hpp"
+#include "program.hpp"
+#include "serializer.hpp"
+#include "cereal/types/memory.hpp"
+#include "entity.hpp"
 #include "renderer.hpp"
 #include "resource_manager.hpp"
 #include "shader.hpp"
-#include "sprite_renderer.hpp"
+#include "components/sprite_renderer.hpp"
 #include "window.hpp"
-
-float triangle[] = {
-    //Vertex data    
-    0.5f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f, 
-    0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-    -0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 1.0f,
-};
-unsigned int indices[] = {
-    0, 1, 3,
-    1, 2, 3
-};
-
-float uSpeed = 1.0f;
-glm::vec2 objPos(0.0f, 0.0f);
-//TODO: create an actual input handler class
-class _input : public IRenderUpdater{
-  public:
-    GLFWwindow* win;
-    SpriteRenderer* sprite;
-  _input(GLFWwindow* window, SpriteRenderer* sprite) : win(window), sprite(sprite){
-    
-  }
-  void update(float deltaTime) override
-  {
-    processInput(this->win);
-  }
-  bool getEnabled() const override
-  {
-    return true;
-  }
-  void processInput(GLFWwindow *window){
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
-
-        glfwSetWindowShouldClose(window, true);
-    }
-    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-        this->sprite->position.x += 1.0f;
-    }
-    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
-        this->sprite->position.x -= 1.0f;
-    }
-    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
-        this->sprite->position.y -= 1.0f;
-    }
-    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
-        this->sprite->position.y += 1.0f;
-    }
-}
-};
+using namespace game;
 int main()
 {
+  auto program = game::Program();
 
-    auto gameWin = new Window(800, 900, "First Steps");
-    ResourceManager& resourceManager = ResourceManager::GetInstance();
-    auto renderer = std::make_shared<Renderer>(gameWin);
+  program.init();
 
-    Shader triangleShader("shaders/vertex.glsl", "shaders/fragment.glsl");
-    float angle = 0.0f;
-    float lastTime = 0.0f;
+  program.gameloop();
 
-    resourceManager.RegisterResource<Renderer>(renderer);
-    auto src = resourceManager.GetResource<Renderer>();
-    std::cout << "Has Value: " << src.has_value() << std::endl;
-    SpriteRenderer* Renderer1 = new SpriteRenderer(triangleShader);
-
-    _input* inputs_temp = new _input(gameWin->glfwWindow, Renderer1);
-    renderer->addUpdater(inputs_temp);
-        unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-
-    //Element Buffer Object
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-
-
-
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_DYNAMIC_DRAW);
-
-
-    // 1. bind Vertex Array Object
-    glBindVertexArray(VAO);
-    // 2. copy our vertices array in a buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_DYNAMIC_DRAW);
-    // 3. then set our vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    renderer->render();
-
-    glfwTerminate();
-    return 0;
+  glfwTerminate();
+  return 0;
 }
 
 
