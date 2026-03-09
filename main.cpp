aa59ed2b3cd4b6d5774a67032078f977932d6e96
Mario Aviles <mjaviles360@gmail.com>
2026-03-08 21:29:54 -0500

    feat: clean up includes

--- main.cpp
+++ main.cpp
@@ -3,20 +3,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <iostream>
-#include <memory>
-#include <vector>
-
-#include "cereal/archives/json.hpp"
 #include "program.hpp"
-#include "serializer.hpp"
-#include "cereal/types/memory.hpp"
-#include "entity.hpp"
-#include "renderer.hpp"
-#include "resource_manager.hpp"
-#include "shader.hpp"
-#include "components/sprite_renderer.hpp"
-#include "window.hpp"
 using namespace game;
 int main()
 {
